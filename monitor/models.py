2beffb295aa0e1cb27da53c77b581ad0e6727b4a
张 志亮 <zhilight@gmail.com>
2016-04-28 14:53:08 +0800

    建立新的主机模型

--- monitor/models.py
+++ monitor/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class DBinfo(models.Model):
     hostname = models.CharField(max_length=200)
     db_type = models.CharField(max_length=10)
+
+
+class Host(models.Model):
+    hostname = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+    ip = models.IntegerField()
