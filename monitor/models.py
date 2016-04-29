1cac3a35bc95c54d5f5a8c574c8a16f88a598214
张 志亮 <zhilight@gmail.com>
2016-04-29 17:38:44 +0800

    提交迁移

--- monitor/models.py
+++ monitor/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class DBinfo(models.Model):
-    db_type = models.CharField(max_length=10)
+    db_type = models.CharField(max_length=100)
+    # create_at = models.DateTimeField('create time')
 
 
 class Host(models.Model):
     hostname = models.CharField(max_length=200)
     position = models.CharField(max_length=200)
-    ip = models.IntegerField()
+    ip = models.IntegerField(default=0)
